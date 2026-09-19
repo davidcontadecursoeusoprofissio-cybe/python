aa73c109b699c216e8121c633f514cdbe3d070f7
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-09-19 18:46:03 -0300

    atualiza

---
+++ main8.py
@@ -0,0 +1,9 @@
+Cliente = input("Seu nome:")
+valorPago = float(input("Valor digitado:"))
+
+Divida_pagar = valorPago - 900
+
+
+
+print(f"Cliente:{Cliente}")
+print(f"Seu pagamento:{valorPago}(troco{Divida_pagar:.2f})")
