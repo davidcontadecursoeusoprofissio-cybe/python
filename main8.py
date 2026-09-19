90c6ac294cd427f8d99e1f0143b023362d0682b4
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-09-19 18:49:50 -0300

    atualiza

--- main8.py
+++ main8.py
@@ -6,4 +6,4 @@ Divida_pagar = valorPago - 900
 
 
 print(f"Cliente:{Cliente}")
-print(f"Seu pagamento:{valorPago}(troco{Divida_pagar:.2f})")
+print(f"Seu pagamento:{valorPago}(troco:{Divida_pagar:.2f})")
