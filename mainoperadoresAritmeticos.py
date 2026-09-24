57334415bf02e46457a03fd43a9c414ed9c736f3
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-09-24 12:28:04 -0300

    atualiza

---
+++ mainoperadoresAritmeticos.py
@@ -0,0 +1,5 @@
+numero = 7
+
+e_par = (numero %2 == 0)
+print(f"O número {numero} é par? {e_par}")
+# 0 = par e 1 = impar.  numero 7 impar da false =  0 = par. true  no 1 = impar e 7 impar
---
+++ mainoperadoresAritmeticos2.py
@@ -0,0 +1,7 @@
+nota = 8.5
+
+passou_com_louvor=7.0 <= nota <= 10.0
+print(passou_com_louvor)
+#maina nota 8.5
+# nota para passar 7.0
+# <= menor < ou igual = 
